e9d9a6bc35814c06850742a562d3ce71ff6464cb
AmandaLW <mrs.amandalynnward@gmail.com>
2020-11-13 12:04:08 -0800

    started a basics practice sheet

---
+++ basics.js
@@ -0,0 +1,14 @@
+// This file is just me learning the ins and outs of vanilla javascript
+
+// Javascript is loosely typed. I don't specify the type and start all variables with var
+
+var number = 3;
+
+function hello(number){
+    var i;
+    for( i = 0; i < number; i++){
+        console.log("You Rock!");
+    }
+}
+
+hello(number);
